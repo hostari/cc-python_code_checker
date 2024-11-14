31b1b462f18d60061ba0ffbf16d8d0de004b9787
Xavi Ablaza <xavi@hostari.com>
2024-11-14 14:20:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  res.send(`This is a Python Code Checker, perform a POST /check with JSON body: {"code": encoded-base64-python-code } and you will get a response.`);
+});
+
+const port = parseInt(process.env.PORT) || 8081;
+app.listen(port, () => {
+  console.log(`app: listening on port ${port}`);
+});
